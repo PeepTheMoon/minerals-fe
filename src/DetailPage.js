88c7209ea6067b178193ae89f1e92643cb826797
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-05-08 17:46:11 -0700

    repaired linking issue

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import request from 'superagent';
+import './App.css';
 
 export default class DetailPage extends Component {
 
--- src/ListPage.js
+++ src/ListPage.js
@@ -1,42 +1,43 @@
 import React, { Component } from 'react';
 import request from 'superagent';
+import { Link } from 'react-router-dom';
+import './App.css';
 
 //heroku site link
 //https://safe-bastion-92853.herokuapp.com/minerals
 
 export default class ListPage extends Component {
 
-state = {
-    minerals: [],
-    searchQuery: ''
-}
+state = { minerals: [] }
 
-async componentDidMount(){
-        const searchParams = new URLSearchParams(window.location.search);
-        const query = searchParams.get('search') || '';
-        this.setState({ searchQuery: query });
+componentDidMount = async () => {
+        const fetchedData = await request.get(`http://localhost:3000/minerals`);
 
-        const response = await request.get(`http://localhost:3000/minerals`)
+        console.log(fetchedData);
 
-        this.setState({ minerals: response.body })  
+        this.setState({ minerals: fetchedData.body })  
       }
 
     render() {
         return (
             <ul>
-            {
+            Mineral List:
+            {   
                 this.state.minerals.map((mineral)=> {
-                    return <li className="minerals">
-                    <h2>{mineral.name}</h2>
+                    return <li className="mineral-object" key={mineral.id}>
+                    <Link to={ `mineral/${mineral.id}` }>
+                        <h2>{mineral.name}</h2>
+                    </Link>
                     <p>Vibrational Frequency: {mineral.vibrates_to}</p>
                     <p>Rarity: {mineral.rarity
-                    ? 'It is rare.'
-                    : 'It easy to find.'}
+                        ? 'It is rare.'
+                        : 'It easy to find.'}
                     </p>
                     <p>Associated Signs: {mineral.associated_signs}</p>
                     <p>Chakra: {mineral.chakra}</p>
-                    </li> })
-                    }
+                    </li> 
+                })
+            }
             </ul>
         )
     }
