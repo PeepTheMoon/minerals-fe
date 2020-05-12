e32f4cb99b88adcb275ab966e5275e9c1c09d43b
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-05-11 17:03:03 -0700

    completed details info on details page

--- src/ListItem.js
+++ src/ListItem.js
@@ -4,8 +4,14 @@ export default class ListItem extends Component {
     render() {
         return (
             <div>
-                <p>{this.props.mineral.name}</p>
-                
+                <li>
+                    <h2>{this.props.mineral.name}</h2>
+                    <p>Vibrational Frequency: {this.props.mineral.vibrates_to}</p>
+                    <p>Rarity: {this.props.mineral.rarity}</p>
+                    <p>Associated Signs: {this.props.mineral.associated_signs}</p>
+                    <p>Chakra: {this.props.mineral.chakra}</p>
+                </li>
+
             </div>
         )
     }
